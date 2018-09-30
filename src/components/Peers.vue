53ac4543d918d11c73d5a26fd25baaa6c4b50fa0
kyu2 <kyu2@apple.com>
2018-09-30 02:49:17 -0700

    Add Peers and report vue.

---
+++ src/components/Peers.vue
@@ -0,0 +1,27 @@
+<template>
+  <GmapMap
+  :center=center
+  :zoom="14"
+  map-type-id="terrain"
+  style="width: 500px; height: 300px"
+  >
+  <GmapMarker
+  :key="index"
+  v-for="(m, index) in markers"
+  :position="m.position"
+  :clickable="true"
+  :draggable="true"
+  @click="center=m.position"
+  />
+  </GmapMap>
+</template>
+
+<script>
+  export default {
+    name: 'Peers'
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Nearby from '@/components/Nearby'
+import Peers from '@/components/Peers'
+import Report from '@/components/Report'
 
 Vue.use(Router)
 
@@ -10,6 +12,16 @@ export default new Router({
       path: '/',
       name: 'Nearby',
       component: Nearby
+    },
+    {
+      path: '/peers',
+      name: 'Peers',
+      component: Peers
+    },
+    {
+      path: '/report',
+      name: 'Police Report',
+      component: Report
     }
   ]
 })
