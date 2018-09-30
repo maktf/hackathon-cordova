50b80f3f874b9957108d155ecc018ef4175034fd
maktf <xhhxhhxhh@163.com>
2018-09-29 21:58:59 -0700

    remove Hello

--- src/App.vue
+++ src/App.vue
@@ -25,48 +25,11 @@
     </v-navigation-drawer>
     <v-toolbar fixed app :clipped-left="clipped">
       <v-toolbar-side-icon @click.stop="drawer = !drawer" light></v-toolbar-side-icon>
-      <v-btn
-        icon
-        light
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        light
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>web</v-icon>
-      </v-btn>
       <v-toolbar-title v-text="title"></v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        icon
-        light
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>menu</v-icon>
-      </v-btn>
     </v-toolbar>
     <v-content>
       <router-view></router-view>
     </v-content>
-    <v-navigation-drawer
-      temporary
-      :right="right"
-      v-model="rightDrawer"
-      fixed
-    >
-      <v-list>
-        <v-list-tile @click="right = !right">
-          <v-list-tile-action>
-            <v-icon light>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
   </v-app>
 </template>
 
@@ -79,13 +42,23 @@
         clipped: false,
         drawer: true,
         items: [{
-          icon: 'bubble_chart',
-          title: 'Inspire'
+          icon: 'visibility',
+          title: 'Nearby'
+        }, {
+          icon: 'accessibility_new',
+          title: 'Peers'
+        }, {
+          icon: 'stars',
+          title: 'Police Report'
+        }, {
+          icon: 'flag',
+          title: 'Report Case'
+        }, {
+          icon: 'question_answer',
+          title: 'User Messaging'
         }],
         miniVariant: false,
-        right: true,
-        rightDrawer: false,
-        title: 'Vuetify.js'
+        title: 'Red Alert'
       }
     },
     created () {
